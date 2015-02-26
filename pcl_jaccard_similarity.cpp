2a2687e50978b6c133619b126d797e5cb47c5dde
Federico Ferri <federico.ferri.it@gmail.com>
2015-02-26 16:10:22 +0100

    print correct usage

--- pcl_jaccard_similarity.cpp
+++ pcl_jaccard_similarity.cpp
@@ -6,16 +6,49 @@
 #include "pcl_union.cpp"
 #undef op
 
+void usage2(const char *self)
+{
+    std::cout << "usage: " << self << " [options] a.pcd b.pcd" << std::endl
+        << "options:" << std::endl
+        << "  --tolerance <t>" << std::endl
+        << "  -t <t>            two points are considered equal if:" << std::endl
+        << "                    |x1-x2|<t && |y1-y2|<t && |z1-z2|<t" << std::endl;
+}
+
+extern double tolerance;
+
 int main(int argc, char **argv)
 {
-    int i = parse_args(argc, argv);
+    int i = 1;
+
+    if(argc < (i + 1))
+    {
+        usage2(argv[0]);
+        exit(1);
+    }
+
+    if(strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--tolerance") == 0)
+    {
+        i++;
+        if(argc < (i + 1))
+        {
+            usage2(argv[0]);
+            exit(1);
+        }
+        tolerance = atof(argv[i++]);
+        std::cout << "info: tolerance set to " << tolerance << std::endl;
+    }
+    else
+    {
+        std::cout << "info: using default tolerance of " << tolerance << std::endl;
+    }
 
     pcl::PointCloud<pcl::PointXYZ> tmp;
     std::set<point> a, b, c_intersection, c_union;
 
-    if(i + 2 >= argc)
+    if(i + 2 != argc)
     {
-        std::cerr << "usage: " << argv[0] << " a.pcd b.pcd" << std::endl;
+        usage2(argv[0]);
         exit(1);
     }
 
