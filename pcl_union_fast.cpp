4a3294964be43ebf40b69e4954daeab84e0e3d77
Federico Ferri <federico.ferri.it@gmail.com>
2015-02-27 22:15:44 +0100

    re-use pcl_union.cpp in pcl_union_fast.cpp

--- pcl_union_fast.cpp
+++ pcl_union_fast.cpp
@@ -1,9 +1,4 @@
-#include "pcl_common.h"
-
-void op(std::set<point>& a, std::set<point>& b, std::set<point>& c)
-{
-    set_union(a.begin(), a.end(), b.begin(), b.end(), std::inserter(c, c.begin()));
-}
+#include "pcl_union.cpp"
 
 int main(int argc, char **argv)
 {
